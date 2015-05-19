4e75db965514efb493b2a88c02109019b3f09506
ross-spencer <all.along.the.watchtower2001@gmail.com>
2015-05-19 15:03:14 +1200

    [documentation] Removing surplus comments.

--- checksumcomparator.py
+++ checksumcomparator.py
@@ -85,12 +85,7 @@ def main():
    #	Usage: 	--csv [droid report]
    #	Handle command line arguments for the script
    
-   #  TODO: Format of checksums in README
    parser = argparse.ArgumentParser(description='Compare two non-identical, or identical checksum files.')
-
-   #TODO: Consider optional and mandatory elements... behaviour might change depending on output...
-   #other options droid csv and rosetta schema
-   #NOTE: class on its own might be used to create a blank import csv with just static options
    parser.add_argument('--check1', help='Checksum file one.', default=False, required=False)
    parser.add_argument('--check2', help='Checksum file two.', default=False, required=False)
    parser.add_argument('--pre', help='Pre-conditioned objects.', default=False, required=False, action="store_true")
